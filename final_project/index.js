0f3c43b78778589110159b563b6788c14951af53
Fairdose <fairdose.dev@gmail.com>
2024-11-19 01:58:57 +0300

    Project scaling

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,28 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",
+    session({
+        secret: "fingerprint_customer",
+        resave: true,
+        saveUninitialized: true
+    })
+)
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    const { username, password } = req.body
+
+    if (username === 'admin' && password === '123456') {
+        req.session.user = req.user;
+        res.send('Authenticated');
+    } else {
+        res.send('Authentication failed');
+    }
 });
- 
-const PORT =5000;
+
+const PORT = 5001;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
